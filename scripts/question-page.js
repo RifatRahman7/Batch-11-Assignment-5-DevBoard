71c8ea7cd1d272bb810728322ade7ae7bad96046
RifatRahman7 <rifatdcian@gmail.com>
2025-03-01 22:27:08 +0600

    Added page changing JS

---
+++ scripts/question-page.js
@@ -0,0 +1,5 @@
+// Back button page change
+const discoverButton = document.getElementById('backButton');
+discoverButton.addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
--- scripts/script-dev-board.js
+++ scripts/script-dev-board.js
@@ -1,3 +1,4 @@
+// color changing button
 const colors = ['lightblue', 'lightgreen', 'lightcoral', 'lightpink', 'lightgray'];
 const changeColorBtn = document.getElementById('changeColorBtn');
 
@@ -5,4 +6,10 @@ let colorIndex = 0;
 changeColorBtn.addEventListener('click', function () {
     document.body.style.backgroundColor = colors[colorIndex];
     colorIndex = (colorIndex + 1) % colors.length;
+});
+
+// Discover button page change
+const discoverButton = document.getElementById('discover-btn');
+discoverButton.addEventListener('click', function () {
+    window.location.href = 'question-page.html';
 });
